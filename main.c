ad16c0c1440e16b6d1ba69e124363e019864b2cf
Zach Cross <me@zachcross.com>
2014-10-24 15:44:12 -0400

    Update main.c
    - Declared & initialized fileTransfer structure.
    - Added call to UserInt function to main.

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+/*
+	CMPET 457
+	Lab 4
+	Fall 2014
+*/
+
+
+// INCLUDES
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -5,9 +13,15 @@
 
 #include "dataStruct.h"
 
+
 int main()
 {
+	// Declare & Initialize Data Structure(s)
+	struct fileTransfer FT;
+	memset(&FT, '0', sizeof(FT));
 
+	// Invoke User Interface
+	UserInt(&FT);
 
-
-}
+	return 0;
+} // END main
