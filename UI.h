9562199bf9e9094913341f2956a1b202ec20cc00
dasbean <ddr5040@gmail.com>
2014-10-24 15:56:03 -0400

    Create UI.h
    Header file needed for UI.c

---
+++ UI.h
@@ -0,0 +1,8 @@
+#include "cons.h"
+
+void CreateMenu(void);
+void UserInput(void);
+void DisplayIP(int curLocationY, int key);
+void DisplaySub(int curLocationY, int key);
+void DisplayLocal(int curLocationY, int key);
+void DisplayDest(int curLocationY, int key);
