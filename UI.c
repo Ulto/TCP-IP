b37bd5808a2fbbf528b86714d483dc53b8c57ac6
dasbean <ddr5040@gmail.com>
2014-10-24 22:03:44 -0400

    Update UI.c
    Added:
    -Menu for sending receiving
    -Validation for IP and Subnet
    -Delete for IP and Subnet
    -#define s

--- UI.c
+++ UI.c
@@ -2,36 +2,69 @@
 
 void UserInt(struct fileTransfer *info)
 {
+	int choice;
 	//initializes  console controls
 	ConsInit();
 	//clears the screen
 	ConsClear();
 	//calls the function to create the menu
-	CreateMenu();
-	
-	while (1)
-	{	
-		//gets the user input 
-		UserInput();
+	CreateMenu(0);
+
+	while (!kbhit());
+	choice = getch();
+
+	switch (choice-'0')
+	{
+	case 1:
+		CreateMenu(2);
+		//Recieving(&info);
+		break;
+	case 2:
+		CreateMenu(1);
+		while (1)
+		{
+			//gets the user input 
+			UserInput(&info);
+		}
+		break;
 	}
 
 	//frees the console
 	ConsExit();
 }
 
-void CreateMenu()
+void CreateMenu(int menu)
 {
-	//creating the menu
+	ConsClear();
 	printf("    Welcome To Our File Transfer Program \n");
 	printf("--------------------------------------------\n");
-	printf("IP Address: ___.___.___.___\n");
-	printf("Subnet Mask: ___.___.___.___\n");
-	printf("Local File: \n");
-	printf("Destination Location: \n");
-	printf("Port: 1500\n");
+	switch (menu)
+	{
+	case 0:
+		//creating the menu
+		printf("What would you like to do?\n");
+		printf("(1) Receiving\n");
+		printf("(2) Sending\n");
+		printf("Choice: ");
+		break;
+	case 1:
+		//creating the menu
+		printf("IP Address: ___.___.___.___\n");
+		printf("Subnet Mask: ___.___.___.___\n");
+		printf("Local File: \n");
+		printf("Destination Location: \n");
+		printf("Port: 1500\n");
+		break;
+	case 2:
+		printf("Please check other computer for status of the transfer\n");
+		printf("Thank you,\n");
+		printf("The 457 File Transfer Team\n");
+		break;
+	}
+
 }
 
-void UserInput()
+void UserInput(struct fileTransfer *info)
 {
 
 	int curLocationY = 2;
@@ -65,22 +98,22 @@ void UserInput()
 		{
 			//the ip line
 		case 2:
-			DisplayIP(curLocationY, key);
+			DisplayIP(curLocationY, key, &info);
 			break;
 			//the subnet line
 		case 3:
-			DisplaySub(curLocationY, key);
+			DisplaySub(curLocationY, key, &info);
 			break;
 			//the file path line
 		case 4:
-			DisplayLocal(curLocationY, key);
+			DisplayLocal(curLocationY, key, &info);
 			break;
 			//the file destination line
 		case 5:
-			DisplayDest(curLocationY, key);
+			DisplayDest(curLocationY, key, &info);
 			break;
 		}
-		
+
 	}
 
 }
@@ -90,46 +123,110 @@ void UserInput()
 * input:
 * curLocation: line up/down position
 * key: the character from the keyboard
+* *info: the data structure that holds all the data
 *
 * return:
 * none
 */
-void DisplayIP(int curLocationY, int key)
+void DisplayIP(int curLocationY, int key, struct fileTransfer *info)
 {
 	int index;
-	static int ipx = 12;
+	static int ipx = ADDRESSMAX;
 	int badFlag = 0;
 	static int count = 0;
 	static int maxcnt = 0;
+	static int actualval = 0;
+	static int maxflag = 0;
+	int done = CHECK;
+	int bad = WRONG;
+	static int goodGo[ADDRESSMAX] = { 0 };
+	int valid = 0;
 
-	//moves cursor to current location
-	ConsMoveCursor(curLocationY, ipx);
-	//if its not a tab keypress
-	if (key != 9)
+	//if delete key is pressed
+	if (key == DELETE)
 	{
-		//makes sure its a digit if not change color to red
-		if (!isdigit(key))
+		//if its at the end of a section but not the end of the line
+		if (count == 0 && maxcnt != 0)
 		{
-			ConsDisplayAttr(curLocationY, ipx, &key, 5);
-			badFlag = 1;
+			//move back one
+			ipx--;
+			//jump over the .
+			count = SECTIONSIZE;
 		}
-		//displays the character if its a number
-		if (badFlag != 1)
+		//if its not at the end
+		if (maxcnt != 0)
 		{
-			ConsDisplayAttr(curLocationY, ipx, &key, 7);
+			//removing one
+			ipx--;
+			count--;
+			maxcnt--;
 		}
-		//increase the x position of the cursor
-		ipx++;
-		//increases the count to determine where the . are
-		count++;
-		//if it hits its max of 3 move one space to skip the .
-		if (count == 3)
+		//moving back one and replacing the _
+		ConsDisplayAttr(curLocationY, ipx, "_", WHITE);
+		ConsMoveCursor(curLocationY, ipx);
+	}
+	else
+	{
+		//moves cursor to current location
+		ConsMoveCursor(curLocationY, ipx);
+		//if its not at the end of the line
+		if (maxcnt != ADDRESSMAX)
 		{
-			count = 0;
-			ipx++;
-			ConsMoveCursor(curLocationY, ipx);
+			//if its not a tab keypress
+			if (key != TAB)
+			{
+				//makes sure its a digit if not change color to red
+				if (!isdigit(key))
+				{
+					ConsDisplayAttr(curLocationY, ipx, &key, RED);
+					badFlag = 1;
+					goodGo[maxcnt] = 0;
+				}
+				//displays the character if its a number
+				if (badFlag != 1)
+				{
+					ConsDisplayAttr(curLocationY, ipx, &key, WHITE);
+					goodGo[maxcnt] = 1;
+				}
+				//increase the x position of the cursor
+				ipx++;
+				//increases the count to determine where the . are
+				count++;
+				maxcnt++;
+				//if the end of the section is reached
+				if (count == SECTIONSIZE)
+				{
+					//goto the next section
+					count = 0;
+					//jump over the .
+					ipx++;
+					//move the cursor
+					ConsMoveCursor(curLocationY, ipx);
+				}
+				//if its at the end
+				if (maxcnt == ADDRESSMAX)
+				{
+					//check to make sure all the characters are valid
+					for (index = 0; index < ADDRESSMAX; index++)
+					{
+						valid += goodGo[index];
+					}
+					//if valid
+					if (valid == ADDRESSMAX)
+					{
+						//print green checkmark
+						ConsDisplayAttr(curLocationY, ipx, &done, GREEN);
+					}
+					else
+					{
+						//print red x
+						ConsDisplayAttr(curLocationY, ipx, &bad, RED);
+					}
+				}
+			}
 		}
 	}
+	
 }
 
 /* DisplaySub(int curLocationY, int key)
@@ -138,43 +235,105 @@ void DisplayIP(int curLocationY, int key)
 * input:
 * curLocation: line up/down position
 * key: the character from the keyboard
+* *info: the data structure that holds all the data
 *
 * return:
 * none
 */
-void DisplaySub(int curLocationY, int key)
+void DisplaySub(int curLocationY, int key, struct fileTransfer *info)
 {
 	int index;
 	static int subx = 13;
 	int badFlag = 0;
 	static int count = 0;
+	static int maxcnt = 0;
+	int done = CHECK;
+	int bad = WRONG;
+	static int goodGo[ADDRESSMAX] = { 0 };
+	int valid = 0;
 
-	//moves cursor to current location
-	ConsMoveCursor(curLocationY, subx);
-	//if its not a tab keypress
-	if (key != 9)
+	//if delete key is pressed
+	if (key == DELETE)
 	{
-		//makes sure its a digit if not change color to red
-		if (!isdigit(key))
+		//if its at the end of a section but not the end of the line
+		if (count == 0 && maxcnt != 0)
 		{
-			ConsDisplayAttr(curLocationY, subx, &key, 5);
-			badFlag = 1;
+			//move back one
+			subx--;
+			//jump over the .
+			count = SECTIONSIZE;
 		}
-		//displays the character if its a number
-		if (badFlag != 1)
+		//if its not at the end
+		if (maxcnt != 0)
 		{
-			ConsDisplayAttr(curLocationY, subx, &key, 7);
+			//removing one
+			subx--;
+			count--;
+			maxcnt--;
 		}
-		//increase the x position of the cursor
-		subx++;
-		//increases the count to determine where the . are
-		count++;
-		//if it hits its max of 3 move one space to skip the .
-		if (count == 3)
+		//moving back one and replacing the _
+		ConsDisplayAttr(curLocationY, subx, "_", WHITE);
+		ConsMoveCursor(curLocationY, subx);
+	}
+	else
+	{
+		//moves cursor to current location
+		ConsMoveCursor(curLocationY, subx);
+		//if its not at the end of the line
+		if (maxcnt < ADDRESSMAX)
 		{
-			count = 0;
-			count++;
-			ConsMoveCursor(curLocationY, subx);
+			//if its not a tab keypress
+			if (key != TAB)
+			{
+				//makes sure its a digit if not change color to red
+				if (!isdigit(key))
+				{
+					ConsDisplayAttr(curLocationY, subx, &key, RED);
+					badFlag = 1;
+					goodGo[maxcnt] = 0;
+				}
+				//displays the character if its a number
+				if (badFlag != 1)
+				{
+					ConsDisplayAttr(curLocationY, subx, &key, WHITE);
+					goodGo[maxcnt] = 1;
+				}
+				//increase the x position of the cursor
+				subx++;
+				//increases the count to determine where the . are
+				count++;
+				maxcnt++;
+				//if the end of the section is reached
+				if (count == SECTIONSIZE)
+				{
+					//goto the next section
+					count = 0;
+					//jump over the .
+					subx++;
+					//move the cursor
+					ConsMoveCursor(curLocationY, subx);
+				}
+				//if its at the end
+				if (maxcnt == ADDRESSMAX)
+				{
+					//check to make sure all the characters are valid
+					for (index = 0; index < 12; index++)
+					{
+						valid += goodGo[index];
+					}
+					//if valid
+					if (valid == ADDRESSMAX)
+					{
+						//print green checkmark
+						ConsDisplayAttr(curLocationY, subx, &done, GREEN);
+					}
+					else
+					{
+						//print red x
+						ConsDisplayAttr(curLocationY, subx, &bad, RED);
+					}
+				}
+			}
 		}
 	}
 }
@@ -185,11 +344,12 @@ void DisplaySub(int curLocationY, int key)
 * input:
 * curLocation: line up/down position
 * key: the character from the keyboard
+* *info: the data structure that holds all the data
 *
 * return:
 * none
 */
-void DisplayLocal(int curLocationY, int key)
+void DisplayLocal(int curLocationY, int key, struct fileTransfer *info)
 {
 	int index;
 	static int localx = 12;
@@ -227,11 +387,12 @@ void DisplayLocal(int curLocationY, int key)
 * input:
 * curLocation: line up/down position
 * key: the character from the keyboard
+* *info: the data structure that holds all the data
 *
 * return:
 * none
 */
-void DisplayDest(int curLocationY, int key)
+void DisplayDest(int curLocationY, int key, struct fileTransfer *info)
 {
 	int index;
 	static int destx = 22;
