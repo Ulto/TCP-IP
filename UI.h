ae2b70c39f6887f886bfccd0722df09b0d0b1cb2
dasbean <ddr5040@gmail.com>
2014-10-24 22:57:51 -0400

    Update UI.h
    Changed the ascii value of X to a char string of X to allow easier printing on the console

--- UI.h
+++ UI.h
@@ -7,7 +7,7 @@
 #define TAB			    9		//tab character ascii value
 #define ADDRESSMAX		12		//ip and subnet max length
 #define CHECK		    251		//checkmark ascii value
-#define WRONG		    88		// x ascii value
+#define WRONG		    "X"		//character string X
 #define SECTIONSIZE		3		//size of each section of ip/subnet
 #define GREEN		    2		//attribute value for green
 #define RED			    4		//attribute value for red
