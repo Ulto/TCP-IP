b4a3d33896ef0626fc6917a869efe83841bd895a
dasbean <ddr5040@gmail.com>
2014-10-24 23:33:31 -0400

    Update UI.h
    Reorganized and added the max file size #define

--- UI.h
+++ UI.h
@@ -3,16 +3,18 @@
 #ifndef UI_H
 #define UI_H
 
-#define DELETEKEY			8		//delete character ascii value
-#define TAB			    9		//tab character ascii value
 #define ADDRESSMAX		12		//ip and subnet max length
-#define CHECK		    251		//checkmark ascii value
-#define WRONG		    "X"		//character string X
+#define MAXFILESIZE		100		//maximum length of the file path
 #define SECTIONSIZE		3		//size of each section of ip/subnet
-#define GREEN		    2		//attribute value for green
-#define RED			    4		//attribute value for red
-#define WHITE		    7		//attribute value for white
 
+#define TAB			    9		//tab character ascii value
+#define CHECK		    251		//checkmark ascii value
+#define WRONG		    "X"		// x ascii value
+#define DELETEKEY		8		//delete character ascii value
+
+#define RED			    4		//attribute value for red
+#define GREEN		    2		//attribute value for green
+#define WHITE		    7		//attribute value for white
 
 void CreateMenu(int);
 void UserInput(struct fileTransfer *info);
@@ -20,5 +22,4 @@ void DisplayIP(int curLocationY, int key, struct fileTransfer *info);
 void DisplaySub(int curLocationY, int key, struct fileTransfer *info);
 void DisplayLocal(int curLocationY, int key, struct fileTransfer *info);
 void DisplayDest(int curLocationY, int key, struct fileTransfer *info);
-
 #endif /* UI_H */
