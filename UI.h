b2bab0696c393f131ddcc3a502c4c336acf4298b
dasbean <ddr5040@gmail.com>
2014-10-24 22:06:16 -0400

    Update UI.h
    added #define
    added data structure to each function

--- UI.h
+++ UI.h
@@ -1,8 +1,19 @@
 #include "cons.h"
 
-void CreateMenu(void);
-void UserInput(void);
-void DisplayIP(int curLocationY, int key);
-void DisplaySub(int curLocationY, int key);
-void DisplayLocal(int curLocationY, int key);
-void DisplayDest(int curLocationY, int key);
+#define DELETE		  8		//delete character ascii value
+#define TAB			    9		//tab character ascii value
+#define ADDRESSMAX	12		//ip and subnet max length
+#define CHECK		    251		//checkmark ascii value
+#define WRONG		    88		// x ascii value
+#define SECTIONSIZE 3		//size of each section of ip/subnet
+#define GREEN		    2		//attribute value for green
+#define RED			    4		//attribute value for red
+#define WHITE		    7		//attribute value for white
+
+
+void CreateMenu(int);
+void UserInput(struct fileTransfer *info);
+void DisplayIP(int curLocationY, int key, struct fileTransfer *info);
+void DisplaySub(int curLocationY, int key, struct fileTransfer *info);
+void DisplayLocal(int curLocationY, int key, struct fileTransfer *info);
+void DisplayDest(int curLocationY, int key, struct fileTransfer *info);
