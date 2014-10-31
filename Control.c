09412f5ffeb968fb41d5dd1cb01181dbe093dacf
Zach Cross <me@zachcross.com>
2014-10-31 15:20:40 -0400

    Update Control.c

--- Control.c
+++ Control.c
@@ -1,12 +1,29 @@
 #include "Control.h"
+#include "dataStruct.h"
+#include "Error.h"
 
 int Control(struct fileTransfer *FT)
 {
+    // Variable Declarations
+	FILE *file_handle;
+	enum ErrorCodes ntwk;
 
+    // Attempt to Open File
+	file_handle = fopen(FT->ui.filePath, "r");
 
+    // Check for File Handle
+	if (file_handle == NULL)
+	{
+        // Could not open file, return error.
+		return(Control_OpenFail);
+	}
 
+    // Store File Handle
+	FT->net.ourFile;
 
+    // Call Network
+	ntwk = NTWK_transmit(FT);
 
-
-	return(1);
+    // Return
+	return(ntwk);
 }
