38073c14b86324aaf6af540f3fa48f05440f764b
Ulto <greg@pohodich.com>
2014-10-31 15:11:46 -0400

    Update Receive.h
    Added new function prototypes

--- Receive.h
+++ Receive.h
@@ -3,6 +3,13 @@
 #ifndef RECEIVE_H
 #define RECEIVE_H
 
+#define TEMP_FILE "Temp.file"
+
 int Receive_Open(struct fileTransfer*);
+int Receive_Write(struct fileTrasnfer *RC);
+int Receive_CompleteTransfer(struct fileTransfer *RC);
+
+
+
 
 #endif /* RECEIVE_H */
