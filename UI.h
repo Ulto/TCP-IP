48d3f8de5d1e94952c9e75577a090efa05c58d8d
dasbean <ddr5040@gmail.com>
2014-10-31 15:15:54 -0400

    Update UI.h
    Added max filename length

--- UI.h
+++ UI.h
@@ -6,6 +6,7 @@
 
 #define ADDRESSMAX		12		//ip and subnet max length
 #define MAXFILESIZE		100		//maximum length of the file path
+#define MAXFILELENGTH	25
 #define SECTIONSIZE		3		//size of each section of ip/subnet
 
 #define TAB			    9		//tab character ascii value
@@ -14,14 +15,18 @@
 #define DELETEKEY		8		//delete character ascii value
 #define ENTERKEY		13
 #define QUESTMARK		63
+#define BACKSLASH		92
 
 #define STARTLOC		22
-#define BADCHAR			{ 92, ':', '*', '?', '"', '<', '>', '|', '^' }
+#define BADCHAR			{ BACKSLASH, '*', '?', '"', '<', '>', '|', '^' }
 
 #define RED			    4		//attribute value for red
 #define GREEN		    2		//attribute value for green
 #define WHITE		    7		//attribute value for white
 #define BLUE			9
+
+#define KEYCHECK		kbhit()
+#define CORRECTDATA		ipFlag == 1 && subFlag == 1 && localFlag == 1 && destFlag == 1 && fileFlag == 1
 
 #define CHECKMARK(id)	ConsDisplayAttr(9+id, 23, &done, GREEN)
 #define RED_X(id)		ConsDisplayAttr(9+id, 23, WRONG, RED)
@@ -31,6 +36,8 @@ void CreateUIMenu(int);
 void UserInput(struct fileTransfer *info);
 int DisplayIP(int curLocationY, int key, struct fileTransfer *info, int doneFlag);
 int DisplaySub(int curLocationY, int key, struct fileTransfer *info, int doneFlag);
+int DisplayFile(int curLocationY, int key, struct fileTransfer *info, int doneFlag);
 int DisplayLocal(int curLocationY, int key, struct fileTransfer *info, int doneFlag);
 int DisplayDest(int curLocationY, int key, struct fileTransfer *info, int doneFlag);
+extern int NTWK_Receive(struct fileTransfer *);
 #endif /* UI_H */
