61defd577414293c3f1c6074e3bfc339a9ccdfd8
Ulto <greg@pohodich.com>
2014-10-31 14:16:59 -0400

    Update Error.h

--- Error.h
+++ Error.h
@@ -1,1 +1,18 @@
 //header file for the error codes
+#ifndef ERROR_H
+#define ERROR_H
+
+enum ErrorCodes{
+    OK = 0,
+	/* Zach Errors - 1xx */
+	placeholder = 100,
+
+	/* Chris Errors - 2xx */
+	placeholder2 = 200,
+
+	/* Greg Errors - 3xx */
+	Err_OpenFail = 300,      /* The file failed to open in the Receive Side */
+
+};
+
+#endif /* ERROR_H */
