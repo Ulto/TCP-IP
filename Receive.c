223f0ab979ec31c75a9b06ab7e34292c237f3a16
Ulto <greg@pohodich.com>
2014-10-31 15:12:14 -0400

    Update Receive.c
    Actually wrote functions

--- Receive.c
+++ Receive.c
@@ -1,12 +1,46 @@
 #include "Receive.h"
+#include "Error.h"
+
+
+
+
+
 
 int Receive_Open(struct fileTransfer *RC)
+{
+	strcpy(RC->ui.destination, TEMP_FILE);
+
+	RC->net.ourFile = fopen(RC->ui.destination, "w+");	/* Open a temporary file with Read/Write plus overwrite */
+
+	if (RC->net.ourFile == NULL)
+		return(Receive_OpenFail);
+
+	return(OK);	/* No Errors, succesful open */
+}
+
+/************************************************************************************/
+int Receive_Write(struct fileTrasnfer *RC)
 {
 
 
 
 
+}
 
+/************************************************************************************/
+int Receive_CompleteTransfer(struct fileTransfer *RC)
+{
+	int err;
 
-	return(1);	/* No Errors, succesful open */
+	err = fclose(RC->net.ourFile);	/* Close the file */
+
+	if (err != 0)
+		return(Receive_CloseFail);
+
+	err = rename(TEMP_FILE, RC->ui.fileName);	/* Rename the file */
+
+	if (err != 0)
+		return(Receive_RenameFail);
+
+	return(OK);	/* No Errors, succesful close and rename */
 }
