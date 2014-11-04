8fb71570d8966102529f1da7eed6eb73987e16a2
Christopher Conklin <cgc.ind@gmail.com>
2014-11-04 13:54:56 -0500

    Update Networking.c
    fix the error checking at the end of the files.

--- Networking.c
+++ Networking.c
@@ -3,138 +3,116 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 int NTWK_Receive(struct fileTransfer *sptr)
 {
-	int errorNTWK = 0;
-	int incomingLength = 0;
+    int errorNTWK = 0;
+    int incomingLength = 0;
 
-	/*Set port number*/
-	sptr->ui.port = 1500;
+    /*Initialize the slave to receive*/
+    errorNTWK = RecvInit(sptr->ui.port);
 
-	/*Initialize the slave to receive*/
-	errorNTWK = RecvInit(sptr->ui.port);
+    /*If no error, then Wait for connection*/
+    if (errorNTWK == 0)
+    {
+        /*Block and Wait for network connection*/
+        NtwkWait();
+    }
 
-	/*If no error, then Wait for connection*/
-	if (errorNTWK == 0)
-	{	
-		/*Block and Wait for network connection*/
-		NtwkWait();
-	}
-	
-	/*If there is no error, enter recieve protocol*/
-	if (errorNTWK == 0)
-	{
+    /*If there is no error, enter recieve protocol*/
+    if (errorNTWK == 0)
+    {
 
-		/*Receive Continuosly*/
-		while (1)
-		{
-			/*Recieve data from the destination*/
-			incomingLength = NtwkRecv(sptr->net.length, sptr->net.buffer);
+        /*Receive Continuously*/
+        while (1)
+        {
+            /*Recieve data from the destination*/
+            incomingLength = NtwkRecv(sizeof(*sptr), sptr);
 
-			/*If incoming length is less then zero, then it's an error code.*/
-			if (incomingLength < 0)
-			{
-				/*Set error code from length error*/
-				errorNTWK = incomingLength;
+            /*If incoming length is less then zero, then it's an error code.*/
+            if (incomingLength < 0)
+            {
+                /*Set error code from length error*/
+                errorNTWK = incomingLength;
 
-				/*Break out of loop*/
-				break;
-			}
+                /*Break out of loop*/
+                break;
+            }
 
-			/*Send packet to send program*/
-			errorNTWK = NtwkSend(sptr->net.length, sptr->net.buffer);
+			errorNTWK = //CALLL GREG !!
 
-			/*If error, then break out of loop*/
-			if (errorNTWK < 0)
-				break;
-		}
-	}
+            /*Send packet to send program*/
+            errorNTWK = NtwkSend(sizeof(errorNTWK), &errorNTWK);
 
-	if (errorNTWK == 0)
-	{
-		/*Exit Network*/
-		errorNTWK = NtwkExit();
-	}
+            /*If error, then break out of loop*/
+            if (errorNTWK < 0)
+                break;
+        }
+    }
 
-	/*If error, then break out of loop*/
-	if (errorNTWK < 0)
-	{
-		/*Create error code*/
-		errorNTWK = 206 - errorNTWK;
-	}
-	return(errorNTWK);
+    if (errorNTWK == 0)
+    {
+        /*Exit Network*/
+        errorNTWK = NtwkExit();
+    }
+
+    /*If error, then break out of loop*/
+    if (errorNTWK < 0)
+    {
+        /*Create error code*/
+        errorNTWK = 206 - errorNTWK;
+    }
+    return(errorNTWK);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 int NTWK_Transmit(struct fileTransfer *sptr)
 {
-	int errorNTWK = 0;
-	/*Initialize host to transmit*/
-	/*Passing in ip address and port number*/
-	errorNTWK = SendInit(sptr->ui.ipAddress, sptr->ui.port);
+    int errorNTWK = 0;
+    /*Initialize host to transmit*/
+    /*Passing in ip address and port number*/
+    errorNTWK = SendInit(sptr->ui.ipAddress, sptr->ui.port);
 
-	/*If no error, then Wait for connection*/
-	if (errorNTWK == 0)
-	{
-		/*Block and Wait for network connection*/
-		NtwkWait();
-	}
+    /*If there is no error, enter recieve protocol*/
+	if (errorNTWK == ERR_NONE)
+    {
+        // obtain file size:
+        fseek(sptr->net.ourFile, 0, SEEK_END);
+        sptr->net.length = ftell(sptr->net.ourFile);
+        rewind(sptr->net.ourFile);
 
-	/*If there is no error, enter recieve protocol*/
-	if (errorNTWK == 0)
-	{
-		sptr->net.ourFile = fopen(sptr->ui.filePath, "r");
-		/*If file is of no size*/
-		if (NULL == sptr->net.ourFile)
-		{
-			// TODO: return a suitable error/perror
-			return -1;
-		}
+        /*open file(send filename to dest)*/
+        while (1)
+        {
+            /*Read the file and store size "length" into "buffer"*/  //fix ///////////////////////////three different cases
+            int datainbuff = fread(sptr->net.buffer, sizeof(char), sizeof(sptr->net.buffer), sptr->net.ourFile);
 
-		// obtain file size:
-		fseek(sptr->net.ourFile, 0, SEEK_END);
-		sptr->net.length = ftell(sptr->net.ourFile);
-		rewind(sptr->net.ourFile);
+            /*Send 1500 bytes to reciever*/  //Update data structure 
+            errorNTWK = NtwkSend(sizeof(*sptr), sptr);
 
-		/*open file(send filename to dest)*/
-		while (1)
-		{
-			/*Read the file and store size "length" into "buffer"*/
-			fread(sptr->net.buffer, sizeof(char), sptr->net.length, sptr->net.ourFile);
+            /*If Error*/
+            if (errorNTWK < 0)
+                break;
 
-			/*Send 1500 bytes to reciever*/
-			errorNTWK = NtwkSend(sptr->net.length, sptr->net.buffer);
+            /*Call to wait for a response*/
+            errorNTWK = NtwkRecv(sizeof(sptr->ERR), &sptr->ERR);
 
-			/*If Error*/
-			if (errorNTWK < 0)
-				break;
+            /*Error*/ 
+            if (errorNTWK < 0  || sptr->ERR != OK)
+                break;
+        }
+        fclose(sptr->net.ourFile);
+    }
 
-			/*Call to wait for a response*/
-			errorNTWK = NtwkRecv(sptr->net.length, sptr->net.buffer);
+    if (errorNTWK == 0)
+    {
+        /*Exit Network*/
+        errorNTWK = NtwkExit();
+    }
 
-			/*If end of file , then break*/
-			if (feof(sptr->net.ourFile))
-			{
-				break;
-			}
-
-			/*Error*/
-			if (errorNTWK < 0)
-				break;
-		}
-		fclose(sptr->net.ourFile);
-	}
-
-	if (errorNTWK == 0)
-	{
-		/*Exit Network*/
-		errorNTWK = NtwkExit();
-	}
-
-	/*If error, then break out of loop*/
-	if (errorNTWK < 0)
-	{
-		/*Create error code*/
-		errorNTWK = 206 - errorNTWK;
-	}
-	return(errorNTWK);
+    /*If error, then break out of loop*/
+    if (errorNTWK < 0)
+    {
+        /*Create error code*/
+        errorNTWK = 206 - errorNTWK;
+    }
+    return(errorNTWK);
 }
 
