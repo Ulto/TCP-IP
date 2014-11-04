ebb91c1f6df9a39b86ce821af4b0d9da05795ea8
Christopher Conklin <cgc.ind@gmail.com>
2014-11-04 13:27:44 -0500

    Sample review updates (control.c)
    - return enum
    - remove multiple returns

--- Control.c
+++ Control.c
@@ -2,28 +2,32 @@
 #include "dataStruct.h"
 #include "Error.h"
 
-int Control(struct fileTransfer *FT)
+enum Control(struct fileTransfer *FT)
 {
     // Variable Declarations
-	FILE *file_handle;
-	enum ErrorCodes ntwk;
+    FILE *file_handle;
+    enum ErrorCodes ntwk;
 
     // Attempt to Open File
 	file_handle = fopen(FT->ui.filePath, "r");
+					// Must concat path & name
 
     // Check for File Handle
-	if (file_handle == NULL)
-	{
+    if (file_handle == NULL)
+    {
         // Could not open file, return error.
-		return(Control_OpenFail);
+		ntwk = Control_OpenFail;
+    }
+	else
+	{
+
+		// Store File Handle
+		FT->net.ourFile = file_handle;
+
+		// Call Network
+		ntwk = NTWK_transmit(FT);
 	}
 
-    // Store File Handle
-	FT->net.ourFile;
-
-    // Call Network
-	ntwk = NTWK_transmit(FT);
-
     // Return
-	return(ntwk);
+    return(ntwk);
 }
