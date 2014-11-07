a84a29e679bdc7a2e1cd8f534338415f5d2e7bf1
Christopher Conklin <cgc.ind@gmail.com>
2014-11-07 11:53:07 -0500

    Create networking.h

---
+++ networking.h
@@ -0,0 +1,9 @@
+#include"ntwk.h"
+#include "networking.h" 
+
+/*Network recieve*/
+int NTWK_Receive(struct fileTransfer *sptr);
+
+/*Network transmit*/
+int NTWK_Transmit(struct fileTransfer *sptr);
+
