19d4cabeadd0ae920188df6a30f37fa14b964c3e
Christopher Conklin <cgc.ind@gmail.com>
2014-11-07 12:59:07 -0500

    Update NTWK_header.h

--- NTWK_header.h
+++ NTWK_header.h
@@ -8,3 +8,16 @@ int NTWK_Receive(struct fileTransfer *sptr);
 /*Network transmit*/
 int NTWK_Transmit(struct fileTransfer *sptr);
 
+int SendInit(char *ip_addrs, int port);
+
+int RecvInit(int port);
+
+int NtwkWait(void);
+
+int NtwkExit(void);
+
+int NtwkSend(int length, unsigned char *datap);
+
+int NtwkRecv(int length, unsigned char *datap);
+
+
