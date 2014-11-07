7f683841e3043cee6146e6e220cf1b60e61af420
Ulto <greg@pohodich.com>
2014-11-07 12:51:46 -0500

    Update Receive.h
    Changed return types to enum

--- Receive.h
+++ Receive.h
@@ -5,9 +5,9 @@
 
 #define TEMP_FILE "Temp.file"
 
-int Receive_Open(struct fileTransfer*);
-int Receive_Write(struct fileTrasnfer *RC);
-int Receive_CompleteTransfer(struct fileTransfer *RC);
+enum Receive_Open(struct fileTransfer*);
+enum Receive_Write(struct fileTransfer*);
+enum Receive_CompleteTransfer(struct fileTransfer*);
 BOOL Receive_fileExists(struct fileTransfer*);
 
 
