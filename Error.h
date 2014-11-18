1656e6dcdee796aef85885204b927d5b00f1d3c0
Ulto <greg@pohodich.com>
2014-11-18 16:10:15 -0500

    Update Error.h
    Fixed typos

--- Error.h
+++ Error.h
@@ -11,18 +11,18 @@ enum ErrorCodes{
 	/* Chris Errors - 2xx */
 	placeholder2 = 200,
 	Ntwk_ERR_SEND_RECV,         /* Data didn't reach destination */
-	Ntwk_ERR_CONN_FAIL,         /* Failed to make connection network */
-	Ntwk_ERR_NTWK_FAIL,         /* Miscelaneous network failure */
+	Ntwk_ERR_CONN_FAIL,         /* Failed to make connection to network */
+	Ntwk_ERR_NTWK_FAIL,         /* Miscellaneous network failure */
 	Ntwk_ERR_BAD_SEQ,           /* No socket connection */
 	Ntwk_ERR_BAD_IP,            /* Invalid network ip address */
 
 	/* Greg Errors - 3xx */
 	Receive_OpenFail = 300,     /* The file failed to open in the Receive Side */
-    Receive_CloseFail,          /* File failed to close in Receive Side */
+    	Receive_CloseFail,          /* File failed to close in Receive Side */
 	Receive_RenameFail,         /* File failed to rename in Receive Side */
 	Receive_FileAlreadyExists,  /* File already exists in Receive Side */
 	Receive_FileWriteFail,      /* During a File write, the system failed to write all data */
-	Receive_Done,               /* The file was succesfully closed and renamed */
+	Receive_Done,               /* The file was successfully closed and renamed */
 	Receive_UpdateTimeFail      /* Failed to update file times */
 
 };
